2ff65c065017570498ec1e527a16dd593351e8b5
janamoumita1997 <jana12moumita@gmai.com>
2026-05-16 23:59:51 +0530

    --

--- Markar.py
+++ Markar.py
@@ -2,6 +2,7 @@ from marker.converters.pdf import PdfConverter
 from marker.models import create_model_dict
 from marker.output import text_from_rendered
 
+
 # Load models + convert
 converter = PdfConverter(artifact_dict=create_model_dict())
 rendered = converter("output.pdf")
