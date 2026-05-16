2e340f7d97d0f31e96743cdbe69a5aaa94e279f3
janamoumita1997 <jana12moumita@gmai.com>
2026-05-17 00:01:33 +0530

    --

--- Markar.py
+++ Markar.py
@@ -5,7 +5,7 @@ from marker.output import text_from_rendered
 
 # Load models + convert
 converter = PdfConverter(artifact_dict=create_model_dict())
-rendered = converter("output.pdf")
+rendered = converter('output.pdf')
 
 # Extract text
 text, _, images = text_from_rendered(rendered)
