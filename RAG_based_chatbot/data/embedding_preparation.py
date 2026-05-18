1c2c38dcced24cfe58990e4ff61f4fae17f0f8b7
janamoumita1997 <jana12moumita@gmail.com>
2026-05-18 23:03:59 +0530

    --

--- RAG_based_chatbot/data/embedding_preparation.py
+++ RAG_based_chatbot/data/embedding_preparation.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter   
-import os
+import os,re
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 pdf_path = os.path.join(current_dir, "jesc101.pdf")
@@ -9,7 +9,33 @@ loader = PyPDFLoader(pdf_path)
 docs = loader.load()
 # print(docs)
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, separators=["\n\n", "\n", " ", ""])
+# splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, separators=["\n\n", "\n", " ", ""])
 
-chunks = splitter.split_documents(docs)
-print(chunks)
+# chunks = splitter.split_documents(docs)
+# print(chunks)
+
+def chunk_book(text, chunk_size=500, chunk_overlap=50):
+
+    # Step 1 — split by section headers first
+    section_pattern = r'(?=\n\d+(\.\d+)*[\s\t])'
+    sections = re.split(section_pattern, text)
+    sections = [s.strip() for s in sections if s and s.strip()]
+
+    # Step 2 — further split large sections using RecursiveCharacterTextSplitter
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", " ", ""]
+    )
+
+    final_chunks = []
+    for section in sections:
+        if len(section) > chunk_size:
+            # large section → split further
+            sub_chunks = splitter.split_text(section)
+            final_chunks.extend(sub_chunks)
+        else:
+            # small section → keep as one chunk
+            final_chunks.append(section)
+
+    return final_chunks
